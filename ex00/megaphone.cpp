e64652c1b573108abaaaf6603117d066045e4684
Megmi <doseisan274@outlook.jp>
2025-12-11 02:12:27 +0900

    Rewrite_in_C++style

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -10,9 +10,10 @@ int main(int argc, char **argv)
 
     for (int i = 1; i < argc; i++) // argv[0]はプログラム名なので1から開始
     {
-        for (int j = 0; argv[i][j]; j++) // 文字列の各文字を処理
+        std::string str = argv[i];
+        for (size_t j = 0; j < str.length(); j++) //C++styleヌル終端文字列の扱い（std::string を使用、str.length() で文字列の長さを取得
         {
-            std::cout << (char)std::toupper(argv[i][j]);
+            std::cout << static_cast<char>(std::toupper(argv[i][j]));//C++の推奨style:型変換が明示的、コード意図が明確に
         }
     }
     std::cout << std::endl;
